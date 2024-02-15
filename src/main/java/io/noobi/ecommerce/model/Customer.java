68333dd95835257c20f7bf6f23adbdd824f0d19d
ragul-engg <ragulshanmugarajan21@gmail.com>
2024-02-15 23:25:16 +0530

    update customer and order entity for relationship

--- src/main/java/io/noobi/ecommerce/model/Customer.java
+++ src/main/java/io/noobi/ecommerce/model/Customer.java
@@ -1,6 +1,14 @@
 package io.noobi.ecommerce.model;
 
-import jakarta.persistence.*;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,19 +19,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Customer {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-  @Column(name = "first_name")
-  private String firstName;
+	@Column(name = "first_name")
+	private String firstName;
 
-  @Column(name = "last_name")
-  private String lastName;
+	@Column(name = "last_name")
+	private String lastName;
 
-  @Column(name = "phone")
-  private Long mobile;
+	@Column(name = "phone")
+	private Long mobile;
 
-  private String email;
+	private String email;
 
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "customer")
+	private List<Order> orders;
 }
--- src/main/java/io/noobi/ecommerce/model/Order.java
+++ src/main/java/io/noobi/ecommerce/model/Order.java
@@ -1,6 +1,12 @@
 package io.noobi.ecommerce.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,14 +17,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Order {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-  private int quantity;
+	private int quantity;
 
-  private double price;
+	private double price;
 
-  @Column(name = "total_price")
-  private double totalPrice;
+	@Column(name = "total_price")
+	private double totalPrice;
+
+	@ManyToOne
+	@JoinColumn(name = "customer_id", nullable = false)
+	private Customer customer;
 }
